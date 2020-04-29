74cb091a72f9f698add259997528f1d35c0e0182
r0b0t <antoniomainakn@gmail.com>
2020-04-29 16:04:33 +0300

    chore: set up env variables

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 from flask import Flask, jsonify
 from flask_restful import Api, Resource
 from spiders.who import who_spider
+from dotenv import load_dotenv
+import os
+import secrets
 
+# Load environment variables from .env file
+load_dotenv()
+
+# Initialize flask application
 app = Flask(__name__)
+app.config["DEBUG"] = os.getenv("DEBUG", False)
+app.config["SECRET_KEY"] = secrets.token_hex()
+
+# Initialize Api
 api = Api(app)
 
 
 class FaqsResource(Resource):
+    """
+    Returns a dictionary of all faqs from various spiders.
+    """
+
     def get(self):
         who_faqs = who_spider()
         return jsonify(who_faqs)
